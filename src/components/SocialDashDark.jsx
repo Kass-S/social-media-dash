aac5724e807cb9a49c63cba1cffce12f83a22bac
Kass-S <kassiespacek101@gmail.com>
2025-03-07 09:36:17 -0800

    altered spacing

--- src/components/SocialDashDark.jsx
+++ src/components/SocialDashDark.jsx
@@ -5,122 +5,120 @@ const SocialDashDark = () => {
 
   return (
     <div className='dark-bg h-[100vh]'>
-        <div className='dark-top-bg'>
+        <div className='dark-top-bg mx-44'>
             <p className='text-white text-2xl'>Social Media Dashboard</p>
             <p className='dark-text'>Total Followers: 23,004</p>
             <p className='dark-text'>Dark Mode</p>
         </div>
 
-        <div className='grid grid-cols-4'>
-            <Card className="max-w-sm dark-card-bg">
-                <h5 className="text-2xl font-bold tracking-tight ">
+        <div className='grid grid-cols-4 mx-44 gap-6 my-10'>
+            <Card className="max-w-sm dark-card-bg border-none">
+                <h5 className="text-2xl font-bold tracking-tight dark-text">
                     @nathanf
                 </h5>
-                <p className="font-normal ">
-                FOLLOWERS                
+                <p className="font-normal dark-text">
+                    FOLLOWERS                
                 </p>
             </Card>
 
-            <Card className="max-w-sm dark-card-bg">
-                <h5 className="text-2xl font-bold tracking-tight ">
+            <Card className="max-w-sm dark-card-bg border-none">
+                <h5 className="text-2xl font-bold tracking-tight dark-text">
                     @nathanf
                 </h5>
-                <p className="font-normal ">
+                <p className="font-normal dark-text">
                     FOLLOWERS
                 </p>
             </Card>
 
-            <Card className="max-w-sm dark-card-bg">
-                <h5 className="text-2xl font-bold tracking-tight ">
+            <Card className="max-w-sm dark-card-bg border-none">
+                <h5 className="text-2xl font-bold tracking-tight dark-text">
                     @realnathanf
                 </h5>
-                <p className="font-normal ">
+                <p className="font-normal dark-text">
                     FOLLOWERS
                 </p>
             </Card>
 
-            <Card className="max-w-sm dark-card-bg">
-                <h5 className="text-2xl font-bold tracking-tight ">
+            <Card className="max-w-sm dark-card-bg border-none">
+                <h5 className="text-2xl font-bold tracking-tight dark-text">
                     Nathan F.
                 </h5>
-                <p className="font-normal ">
+                <p className="font-normal dark-text">
                     SUBSCRIBERS
                 </p>
             </Card>
         </div>
 
-        <p className='text-xl text-white'>Overview - Today</p>
+        <p className='text-xl text-white mx-44'>Overview - Today</p>
 
-        <div className='grid grid-cols-4'>
-            <Card className="max-w-sm dark-card-bg">
-                <h5 className="text-2xl font-bold tracking-tight ">
+        <div className='grid grid-cols-4 gap-6 mx-44'>
+            <Card className="max-w-sm dark-card-bg border-none">
+                <h5 className="text-2xl font-bold tracking-tight dark-text">
                     Noteworthy technology acquisitions 2021
                 </h5>
                 <p className="font-normal ">
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                    
                 </p>
             </Card>
 
-            <Card className="max-w-sm dark-card-bg">
-                <h5 className="text-2xl font-bold tracking-tight ">
+            <Card className="max-w-sm dark-card-bg border-none">
+                <h5 className="text-2xl font-bold tracking-tight dark-text">
                     Noteworthy technology acquisitions 2021
                 </h5>
                 <p className="font-normal ">
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
                 </p>
             </Card>
 
-            <Card className="max-w-sm dark-card-bg">
-                <h5 className="text-2xl font-bold tracking-tight ">
+            <Card className="max-w-sm dark-card-bg border-none">
+                <h5 className="text-2xl font-bold tracking-tight dark-text">
                     Noteworthy technology acquisitions 2021
                 </h5>
                 <p className="font-normal ">
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
                 </p>
             </Card>
 
-            <Card className="max-w-sm dark-card-bg">
-                <h5 className="text-2xl font-bold tracking-tight ">
+            <Card className="max-w-sm dark-card-bg border-none">
+                <h5 className="text-2xl font-bold tracking-tight dark-text">
                     Noteworthy technology acquisitions 2021
                 </h5>
                 <p className="font-normal ">
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                    
                 </p>
             </Card>
 
-            <Card className="max-w-sm dark-card-bg">
-                <h5 className="text-2xl font-bold tracking-tight ">
+            <Card className="max-w-sm dark-card-bg border-none">
+                <h5 className="text-2xl font-bold tracking-tight dark-text">
                     Noteworthy technology acquisitions 2021
                 </h5>
                 <p className="font-normal ">
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                    
                 </p>
             </Card>
 
-            <Card className="max-w-sm dark-card-bg">
-                <h5 className="text-2xl font-bold tracking-tight ">
+            <Card className="max-w-sm dark-card-bg border-none">
+                <h5 className="text-2xl font-bold tracking-tight dark-text">
                     Noteworthy technology acquisitions 2021
                 </h5>
                 <p className="font-normal ">
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                    
                 </p>
             </Card>
 
-            <Card className="max-w-sm dark-card-bg">
-                <h5 className="text-2xl font-bold tracking-tight ">
+            <Card className="max-w-sm dark-card-bg border-none">
+                <h5 className="text-2xl font-bold tracking-tight dark-text">
                     Noteworthy technology acquisitions 2021
                 </h5>
                 <p className="font-normal ">
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                    
                 </p>
             </Card>
 
-            <Card className="max-w-sm dark-card-bg">
-                <h5 className="text-2xl font-bold tracking-tight ">
+            <Card className="max-w-sm dark-card-bg border-none">
+                <h5 className="text-2xl font-bold tracking-tight dark-text">
                     Noteworthy technology acquisitions 2021
                 </h5>
                 <p className="font-normal ">
-                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                    
                 </p>
             </Card>
         </div>
